3b067788789e9843ae32e0b23ad4f4ab982def2c
Yazid <yazidbougrine99@gmail.com>
2022-10-09 00:36:09 +0100

    finished backend ?

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const notesRouter = require('./controllers/notes')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
-
+const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 logger.info('connecting to', config.MONGODB_URI)
 
-mongoose.connect(config.MONGODB_URI)
+mongoose
+  .connect(config.MONGODB_URI)
   .then(() => {
     logger.info('connected to MongoDB')
   })
@@ -23,7 +26,8 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
-
+app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
